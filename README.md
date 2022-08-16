4c3b52c41c4ff6f9228757077a2bdc56ff41fe26
itsjustkevin <kevinjchisholm@google.com>
2022-08-16 12:36:51 -0500

    working state

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
-### Scopes needed for token
+### Current limitations
 
+Currently there are no apis available for creating projectsV2.  There is no
+access to projectsV2 through the rest api and it is not yet implemented in the
+graphql api.
+
+This application will create a project, and the project will need to be
+manually converted to projectsV2 through the github UI.
+
+## Requirements
+
+### Authentication
+
+To utilize this application, it is necessary to create a github token with the
+proper scopes necessary to interact with the api.
+
+### Scopes needed for token
+- [x] repo
 - [ ] admin:org
   - [x] write:org
   - [x] read:org
--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 import { Octokit, App } from "octokit";
 import {config} from "./config.js";
+import * as dotenv from 'dotenv'
+dotenv.config()
 
-const octokit = new Octokit({ auth: ``})
+const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN})
 
 const {
   data: { login },
 } = await octokit.rest.users.getAuthenticated();
 console.log("Hello, %s", login);
 
-const generateProject = () => {
+const generateProject = (feature) => {
   // create github project
+  octokit.rest.projects.createForOrg({
+    org: config.organization,
+    name: feature
+  })
   // make project private
   // name project
   // Return status and projectId
 }
-const generateIssues = (project, issueList) => {
+const generateIssues = (projectID, issueList) => {
   // return list of generated issues
 }
 
