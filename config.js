2bdc1ab9853fe0849ec555e02620b722edcaa9bb
itsjustkevin <kevinjchisholm@google.com>
2022-08-16 10:06:56 -0500

    initial commit

---
+++ config.js
@@ -0,0 +1,115 @@
+export const config = {
+  organization: 'dart-lang',
+  issues: [
+    {
+      title: 'Write language spec',
+      assignee: 'leafpeterson',
+      phase: '0'
+    },
+    {
+      title: 'Language tests',
+      assignee: 'leafpeterson',
+      phase: '1'
+    },
+    {
+      title: 'co19 tests - add tests',
+      assignee: 'leafpeterson',
+      phase: '1'
+    },
+    {
+      title: 'Implement features in the CFE',
+      assignee: 'johnniwinther',
+      phase: '2'
+    },
+    {
+      title: 'Analyzer implementation',
+      assignee: 'srawlins',
+      phase: '2'
+    },
+    {
+      title: 'Tests validating as passing',
+      assignee: 'leafpeterson',
+      phase: '3'
+    },
+    {
+      title: 'Syntax highlighters implementation',
+      assignee: 'domesticmouse',
+      phase: '3'
+    },
+    {
+      title: 'DDC implementation',
+      assignee: 'sigmundch',
+      phase: '3'
+    },
+    {
+      title: 'Dart2js implementation',
+      assignee: 'sigmundch',
+      phase: '3'
+    },
+    {
+      title: 'VM implementation',
+      assignee: 'a-siva',
+      phase: '3'
+    },
+    {
+      title: 'Build system support',
+      assignee: 'jakemac53',
+      phase: '3'
+    },
+    {
+      title: 'Web debugging support',
+      assignee: 'elliette',
+      phase: '3'
+    },
+    {
+      title: 'Core library updates',
+      assignee: 'lrhn',
+      phase: '3'
+    },
+    {
+      title: 'Analysis server / IDE support work',
+      assignee: 'srawlins',
+      phase: '3'
+    },
+    {
+      title: 'dart_style support',
+      assignee: 'munificent',
+      phase: '3'
+    },
+    {
+      title: 'Dartdoc implementation',
+      assignee: 'srawlins',
+      phase: '3'
+    },
+    {
+      title: 'Cider support',
+      assignee: 'keertip',
+      phase: '3'
+    },
+    {
+      title: 'Kythe implementation',
+      assignee: 'iinozemtsev',
+      phase: '3'
+    },
+    {
+      title: 'Angular compiler implementation',
+      assignee: 'grouma',
+      phase: '3'
+    },
+    {
+      title: 'IntelliJ implementation',
+      assignee: 'jwren',
+      phase: '3'
+    },
+    {
+      title: 'Language feature docs',
+      assignee: 'sfshaza2',
+      phase: '3'
+    },
+    {
+      title: 'VM service protocol support',
+      assignee: 'bkonyi',
+      phase: '3'
+    },
+  ]
+}
---
+++ index.js
@@ -0,0 +1,13 @@
+import { Octokit, App } from "octokit";
+import {config} from "./config.js";
+
+const generateProject = () => {
+  // create github project
+  // make project private
+  // name project
+  // Return status and projectId
+}
+const generateIssues = (project, issueList) => {
+  // return list of generated issues
+}
+
